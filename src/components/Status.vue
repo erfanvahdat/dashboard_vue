c3d6b2f1ebdda985e3b10f127d3ccd071bc05041
_erfanvahdat_ <erfanvahdat.6@gmail.com>
2024-10-21 14:32:54 +0330

    add some variable

--- src/components/Status.vue
+++ src/components/Status.vue
@@ -1,24 +1,34 @@
 <template>
   <!-- Status Section -->
   <div
-    class="flex flex-col gap-2 p-3 mt-2 h-full w-[250px] overflow-hidden justify-between font-mono bg-gray-700 rounded">
+    class="flex flex-col gap-2 p-3 mt-2 h-full w-[300px] overflow-hidden justify-between font-mono bg-gray-700 rounded">
 
     <div class="flex flex-col justify-between h-full">
-      <ul class="text-sm space-y-4 w-full p-2 ">
+      <ul class="text-sm space-y-7 w-full p-1 ">
+
 
         <li class="flex justify-between">
           <div class="underline">Balance</div>
-          <div>{{ balance }} <span>USDT</span></div>
+          <div>{{ balance }} <span>USDT</span>
+          </div>
         </li>
+
 
         <li class="flex justify-between">
           <div class="underline">Equity</div>
           <div>{{ equity }} <span>USDT</span></div>
         </li>
 
+
+        <li class="flex justify-between">
+          <div class="underline">Avaialbe_margin</div>
+          <div>{{ available_margin }} <span>USDT</span></div>
+        </li>
+
         <li class="flex justify-between">
           <div class="underline">Unrealized_PnL</div>
-          <div>{{ unProfit }} <span>USDT</span></div>
+          <div>{{ unProfit }} <span>USDT</span>
+          </div>
         </li>
 
         <li class="flex justify-between">
@@ -27,15 +37,14 @@
         </li>
 
         <li class="flex justify-between">
-          <div class="underline">Daily_Profit(%)</div>
-          <div>{{ dailyProfitPercent }} <span>%</span></div>
+          <div class="underline">1_Week profit(USDT)</div>
+          <div>{{ WeeklyPorfit }} <span>USDT</span></div>
         </li>
+
 
       </ul>
     </div>
   </div>
-
-
 
 
 </template>
@@ -44,6 +53,10 @@
 export default {
   props: {
     balance: {
+      type: Number,
+      required: true
+    },
+    available_margin: {
       type: Number,
       required: true
     },
@@ -60,6 +73,10 @@ export default {
       required: true
     },
     dailyProfitPercent: {
+      type: Number,
+      required: true
+    },
+    WeeklyPorfit: {
       type: Number,
       required: true
     }
